11f65f5c702fb1290848f8a6113af36b1b0fc535
Burnt Val <val@burnt.com>
2023-12-12 13:10:00 -0500

    loading screen title/subtitle styling fix

--- packages/abstraxion/src/components/Abstraxion/index.tsx
+++ packages/abstraxion/src/components/Abstraxion/index.tsx
@@ -2,24 +2,18 @@ import { useContext, useEffect, useRef } from "react";
 import { GrazProvider } from "graz";
 import { StytchProvider } from "@stytch/nextjs";
 import { ApolloProvider } from "@apollo/client";
+import { Dialog, DialogContent } from "@burnt-labs/ui";
 import {
   AbstraxionContext,
   AbstraxionContextProps,
   AbstraxionContextProvider,
 } from "../AbstraxionContext";
 import { apolloClient, stytchClient } from "../../lib";
-import { ModalAnchor, Modal } from "@burnt-labs/ui";
 import { AbstraxionSignin } from "../AbstraxionSignin";
 import { useAbstraxionAccount } from "../../hooks";
 import { Loading } from "../Loading";
 import { AbstraxionWallets } from "../AbstraxionWallets";
 import { ErrorDisplay } from "../ErrorDisplay";
-import { Dialog } from "@burnt-labs/ui";
-import { DialogTrigger } from "@burnt-labs/ui";
-import { DialogContent } from "@burnt-labs/ui";
-import { DialogHeader } from "@burnt-labs/ui";
-import { DialogTitle } from "@burnt-labs/ui";
-import { DialogDescription } from "@burnt-labs/ui";
 
 export interface ModalProps {
   onClose: VoidFunction;
