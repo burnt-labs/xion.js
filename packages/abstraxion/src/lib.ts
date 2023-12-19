afe6ec10f70a1c2c12897c117a660a4b97e73bd1
Justin <328965+justinbarry@users.noreply.github.com>
2023-12-19 13:00:45 -0800

    Update lib.ts to use test project

--- packages/abstraxion/src/lib.ts
+++ packages/abstraxion/src/lib.ts
@@ -4,7 +4,7 @@ import { StytchHeadlessClient } from "@stytch/vanilla-js/dist/index.headless";
 
 // TODO: Temporarily hard-coded
 export const stytchClient: StytchHeadlessClient = createStytchHeadlessClient(
-  "public-token-live-59266a62-2720-4452-8984-b65cd54c8838",
+  "public-token-test-317adb7b-a52d-4e45-8c2a-d7dab1ef10a0",
 );
 
 // TODO: Refactor to be dynamic. Local dev uri must be device IP.
