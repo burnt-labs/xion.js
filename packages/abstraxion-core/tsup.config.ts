a9a882a23ff3227591287e7dc28438f7644a7bfa
Kehinde Faleye <Kenny.fale.kf@gmail.com>
2024-02-19 02:39:43 +0100

    refractor: move out grantee signer wallet into a package (#92)
    ---------
    
    Co-authored-by: justin <328965+justinbarry@users.noreply.github.com>

---
+++ packages/abstraxion-core/tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig, Options } from "tsup";
+
+export default defineConfig((options: Options) => ({
+  treeshake: true,
+  splitting: true,
+  entry: ["src/index.ts"],
+  format: ["esm", "cjs"],
+  dts: true,
+  minify: false,
+  clean: true,
+  ...options,
+}));
