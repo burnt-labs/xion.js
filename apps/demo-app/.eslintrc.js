7ef27731b2c000049b73ce532541ea47916978aa
Burnt Nerve <seanlbray@gmail.com>
2024-02-23 09:45:26 -0600

    Added sign arb demo to demo app

--- apps/demo-app/.eslintrc.js
+++ apps/demo-app/.eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
   extends: ["@burnt-labs/eslint-config-custom/next"],
+  rules: {
+    "@typescript-eslint/no-misused-promises": [
+      2,
+      {
+        checksVoidReturn: {
+          attributes: false,
+        },
+      },
+    ],
+  },
 };
