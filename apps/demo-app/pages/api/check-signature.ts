cff222a084db97a9c8ac9bf97f8699df6d2f8452
Justin <328965+justinbarry@users.noreply.github.com>
2024-06-25 13:25:49 -0700

    Add signature and grant verification in API
    The code updates include functions for verifying the XION signature and for checking if specified grants have been given. This verification occurs in the API handler for receiving GET requests, consolidating this functionality in a clearer manner.

--- apps/demo-app/pages/api/check-signature.ts
+++ apps/demo-app/pages/api/check-signature.ts
@@ -3,84 +3,102 @@ import { verifyADR36Amino } from "@keplr-wallet/cosmos";
 
 // This import will need to change based on the chain you are confirming against.
 import { testnetChainInfo } from "@burnt-labs/constants";
-import { CosmWasmClient } from "@cosmjs/cosmwasm-stargate";
-import { Pubkey } from "@cosmjs/amino";
+import { QueryGrantsResponse } from "cosmjs-types/cosmos/authz/v1beta1/query";
 
-interface GrantsResponse {
-  grants: Grant[];
-  pagination: Pagination;
-}
-
-interface Grant {
-  granter: string;
-  grantee: string;
-  authorization: Authorization;
-  expiration: string;
-}
-
-interface Authorization {
-  "@type": string;
-  grants: GrantAuthorization[];
-}
-
-interface GrantAuthorization {
-  contract: string;
-  limit: Limit;
-  filter: Filter;
-}
-
-interface Limit {
-  "@type": string;
-  remaining: string;
-}
-
-interface Filter {
-  "@type": string;
-}
-
-interface Pagination {
-  next_key: null | string;
-  total: string;
-}
-
-function isString(test: any): test is string {
+function isString(test: unknown): test is string {
   return typeof test === "string";
 }
 
-async function checkGrants(granter: string, grantee: string): Promise<Boolean> {
-  const res = await fetch(
-    `${testnetChainInfo.rest}/cosmos/authz/v1beta1/grants/grantee/${granter}`,
-    {
-      cache: "no-store",
-    },
-  );
-  const data = (await res.json()) as GrantsResponse;
-  return data.grants.map((grant) => grant.grantee).includes(grantee);
-}
-
-function checkSignature(
+/**
+ * Verify that the given XION signature corresponds to the given message and address.
+ *
+ * @param address - The address that is supposed to have signed the message.
+ * @param pubKey - The public key associated with the session address base64 encoded.
+ * @param messageString - The message that is supposed to have been signed.
+ * @param signature - The signature to verify against the message and address.
+ * @returns True if the signature is valid, false otherwise.
+ */
+export function verifyXionSignature(
   address: string,
-  pubKey: Pubkey,
+  pubKey: string,
   messageString: string,
   signature: string,
 ): boolean {
-  const msg = Buffer.from(messageString, "hex").toString();
-
   const signatureBuffer = Buffer.from(signature, "base64");
-
   const uint8Signature = new Uint8Array(signatureBuffer); // Convert the buffer to an Uint8Array
-
   const pubKeyValueBuffer = Buffer.from(pubKey, "base64"); // Decode the base64 encoded value
-
   const pubKeyUint8Array = new Uint8Array(pubKeyValueBuffer); // Convert the buffer to an Uint8Array
 
   return verifyADR36Amino(
-    testnetChainInfo.bech32Config.bech32PrefixAccAddr,
+    "xion",
     address,
-    msg,
+    messageString,
     pubKeyUint8Array,
     uint8Signature,
   );
+}
+
+/**
+ * Verifies the Xion signature and grants for a given address, session address, public key,
+ * message string, and signature.
+ *
+ * @param address - The address to verify the grants for.
+ * @param sessionAddress - The session address to verify the grants for.
+ * @param pubKey - The public key associated with the session address base64 encoded.
+ * @param messageString - The message string to verify the signature against.
+ * @param signature - The signature to verify.
+ *
+ * @returns Promise<boolean> - A promise that resolves to true if the Xion signature and grants are valid,
+ *                             or false otherwise.
+ */
+export async function verifyXionSignatureAndGrants(
+  address: string,
+  sessionAddress: string,
+  pubKey: string,
+  messageString: string,
+  signature: string,
+): Promise<boolean> {
+  const isValid = verifyXionSignature(
+    sessionAddress,
+    pubKey,
+    messageString,
+    signature,
+  );
+  if (!isValid) {
+    return false;
+  }
+
+  return verifyGrants(address, sessionAddress);
+}
+
+/**
+ * Verifies if grants have been given by a granter to a grantee.
+ *
+ * @param grantee - The address of the granter.
+ * @param granter - The address of the grantee.
+ *
+ * @return {Promise<boolean>} - A promise that resolves to a boolean indicating whether ANY grants have been given.
+ */
+export async function verifyGrants(
+  granter: string,
+  grantee: string,
+): Promise<boolean> {
+  const baseUrl = `${testnetChainInfo.rest}/cosmos/authz/v1beta1/grants`;
+  const url = new URL(baseUrl);
+  const params = new URLSearchParams({
+    grantee,
+    granter,
+  });
+  url.search = params.toString();
+  const data = await fetch(url, {
+    cache: "no-store",
+  })
+    .then((response): Promise<QueryGrantsResponse> => response.json())
+    .catch((err) => {
+      console.error("Could not fetch grants info", err);
+    });
+
+  return data && data.grants.length > 0;
 }
 
 export default async function handler(
@@ -88,15 +106,23 @@ export default async function handler(
   res: NextApiResponse,
 ) {
   const { query, method } = req;
-  const client = await CosmWasmClient.connect(testnetChainInfo.rpc);
 
   if (method === "GET") {
-    const { userSessionAddress, metaAccountAddress, message, signature } =
-      query;
+    const {
+      userSessionAddress,
+      userSessionPubKey,
+      metaAccountAddress,
+      message,
+      signature,
+    } = query;
 
     const errors: string[] = [];
     if (!userSessionAddress) {
       errors.push("userSessionAddress is required");
+    }
+
+    if (!userSessionPubKey) {
+      errors.push("userSessionPubKey is required");
     }
 
     if (!metaAccountAddress && typeof metaAccountAddress === "string") {
@@ -124,6 +150,13 @@ export default async function handler(
       return;
     }
 
+    if (!isString(userSessionPubKey)) {
+      res
+        .status(400)
+        .json({ errors: ["userSessionPubKey is required to be a string"] });
+      return;
+    }
+
     if (!isString(metaAccountAddress)) {
       res
         .status(400)
@@ -148,39 +181,22 @@ export default async function handler(
      * 1. Confirm the signature passed is valid for the temporary userSessionAddress
      * 2. Pull any grant bestowed to the userSessionAddress on chain
      * 3. Check that AT LEAST one grant exists of any type exists between the userSessionAddress and the metaAccountAddress
+     *
+     * NOTE: If the userSessionAccount has not submitted a transaction to the chain, the PubKey will be unknown.
+     * Therefore it must be passed as a api query parameter (In this example base64 encoded).
+     *
      **/
-    // Need to get the pub key from the chain.
-    const account = await client.getAccount(userSessionAddress);
-    if (!account) {
-      res.status(404).json({
-        errors: ["account not found"],
-      });
-      return;
-    }
-    const { pubkey } = account;
-    if (!pubkey) {
-      res.status(404).json({
-        errors: ["public key not found"],
-      });
-      return;
-    }
 
-    const isValid = checkSignature(
+    const isValid = await verifyXionSignatureAndGrants(
+      metaAccountAddress,
       userSessionAddress,
-      pubkey.value,
+      userSessionPubKey,
       message,
       signature,
     );
-    if (!isValid) {
-      res.status(400).json({
-        errors: ["invalid signature"],
-      });
-      return;
-    }
 
-    // handle GET request
     res.status(200).json({
-      valid: await checkGrants(metaAccountAddress, userSessionAddress),
+      valid: isValid,
     });
   } else {
     res.setHeader("Allow", ["GET"]);
