cff2a3e7eabdb1ab217ea7410ca30abfc81e8401
Burnt Val <117188229+BurntVal@users.noreply.github.com>
2025-02-11 18:18:12 -0500

    Grant check on DAPP startup (#237)
    * implement grant checks for legacy grant configs and treasury contracts
    
    * standardize function names
    
    * changeset
    
    * add tests for compare functions
    
    * lockfile
    
    * contract exec auth comparison
    
    * jsdocs
    
    * Add script to run tests from root of project
    
    * ci: add test job to CI workflow for main branch
    
    * ci: add build step to CI workflow
    
    * fix: update import path for makeADR36AminoSignDoc in SignArbSecp256k1HdWallet test
    
    This was causing tests to fail
    
    ---------
    
    Co-authored-by: Justin <328965+justinbarry@users.noreply.github.com>

--- packages/abstraxion-core/tests/AbstraxionAuth.test.ts
+++ packages/abstraxion-core/tests/AbstraxionAuth.test.ts
@@ -1,17 +1,53 @@
 /**
  * @jest-environment jsdom
  */
-import { AbstraxionAuth } from "../src/AbstraxionAuth";
 import { TextEncoder, TextDecoder } from "text-encoding";
+import { CosmWasmClient } from "@cosmjs/cosmwasm-stargate";
+import { AbstraxionAuth } from "../src/AbstraxionAuth";
+import {
+  mockAccountAddress,
+  mockContractAddress,
+  mockGrantsResponse,
+  mockGrantsResponseForTreasury,
+  mockLegacyConfig,
+} from "./mockData/grantResponses";
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 
+/**
+ * Helper function to configure the AbstraxionAuth instance
+ */
+const configureAbstraxionAuthInstance = (abstraxionAuth: AbstraxionAuth) => {
+  const rpcUrl = "https://testnet-rpc.xion-api.com:443";
+  const restUrl = "https://testnet-api.xion-api.com:443";
+  const grantContracts = mockLegacyConfig.grantContracts;
+  const stake = true;
+  const bank = mockLegacyConfig.bank;
+
+  abstraxionAuth.configureAbstraxionInstance(
+    rpcUrl,
+    restUrl,
+    grantContracts,
+    stake,
+    bank,
+  );
+};
+
 describe("AbstraxionAuth", () => {
   let abstraxionAuth: AbstraxionAuth;
+  let cosmwasmClient: jest.Mocked<CosmWasmClient>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     abstraxionAuth = new AbstraxionAuth();
+
+    cosmwasmClient = jest.createMockFromModule("@cosmjs/cosmwasm-stargate");
+    abstraxionAuth.getCosmWasmClient = jest
+      .fn()
+      .mockResolvedValue(cosmwasmClient);
+
+    cosmwasmClient.queryContractSmart = jest.fn();
+    // abstraxionAuth.decodeAuthorization = jest.fn();
   });
 
   afterEach(() => {
@@ -24,41 +60,10 @@ describe("AbstraxionAuth", () => {
     });
   });
 
-  describe("configureAbstraxionInstance", () => {
-    it("should set configuration properties", () => {
-      const rpcUrl = "https://testnet-rpc.xion-api.com:443";
-      const restUrl = "https://testnet-api.xion-api.com:443";
-      const grantContracts = [
-        {
-          address:
-            "xion1z70cvc08qv5764zeg3dykcyymj5z6nu4sqr7x8vl4zjef2gyp69s9mmdka",
-          amounts: [{ denom: "uxion", amount: "1000000" }],
-        },
-      ];
-      const stake = true;
-      const bank = [
-        {
-          denom: "uxion",
-          amount: "1000000",
-        },
-      ];
-
-      abstraxionAuth.configureAbstraxionInstance(
-        rpcUrl,
-        restUrl,
-        grantContracts,
-        stake,
-        bank,
-      );
-
-      expect(abstraxionAuth.grantContracts).toEqual(grantContracts);
-      expect(abstraxionAuth.stake).toEqual(stake);
-      expect(abstraxionAuth.bank).toEqual(bank);
-    });
-  });
-
   describe("login", () => {
     it("should generate a new keypair and redirect to the dashboard when neither keypair nor granter exist", async () => {
+      configureAbstraxionAuthInstance(abstraxionAuth);
+
       // Simulate undefined keypair and granter
       const getLocalKeypairMock = jest
         .spyOn(abstraxionAuth, "getLocalKeypair")
@@ -74,8 +79,6 @@ describe("AbstraxionAuth", () => {
         } as any);
       const pollForGrantsMock = jest.spyOn(abstraxionAuth, "pollForGrants");
 
-      // TODO: Track redirect
-
       // Call the function
       await abstraxionAuth.login();
 
@@ -89,6 +92,8 @@ describe("AbstraxionAuth", () => {
 
   describe("login", () => {
     it("should poll for grants when both keypair and granter exist", async () => {
+      configureAbstraxionAuthInstance(abstraxionAuth);
+
       // Simulate mock keypair and granter address
       const getLocalKeypairMock = jest
         .spyOn(abstraxionAuth, "getLocalKeypair")
@@ -118,6 +123,143 @@ describe("AbstraxionAuth", () => {
         "granterAddress",
       );
       expect(generateAndStoreTempAccountMock).not.toHaveBeenCalled();
+    });
+  });
+
+  describe("compareGrantsToLegacyConfig", () => {
+    it("should return true when legacy config grants match on-chain grants", () => {
+      configureAbstraxionAuthInstance(abstraxionAuth);
+
+      const result =
+        abstraxionAuth.compareGrantsToLegacyConfig(mockGrantsResponse);
+      expect(result).toBe(true);
+    });
+
+    it("should return false when legacy config grants do not match on-chain grants - bank amount change", () => {
+      configureAbstraxionAuthInstance(abstraxionAuth);
+
+      abstraxionAuth.bank = [{ denom: "uxion", amount: "200" }];
+
+      const result =
+        abstraxionAuth.compareGrantsToLegacyConfig(mockGrantsResponse);
+      expect(result).toBe(false);
+    });
+
+    it("should return false when legacy config grants do not match on-chain grants - grant contracts change", () => {
+      configureAbstraxionAuthInstance(abstraxionAuth);
+
+      abstraxionAuth.grantContracts = [
+        {
+          address: mockAccountAddress,
+          amounts: [{ denom: "uxion", amount: "1000000" }],
+        },
+      ];
+
+      const result =
+        abstraxionAuth.compareGrantsToLegacyConfig(mockGrantsResponse);
+      expect(result).toBe(false);
+    });
+
+    it("should return false when legacy config grants do not match on-chain grants - grant contract spend limit change", () => {
+      configureAbstraxionAuthInstance(abstraxionAuth);
+
+      abstraxionAuth.grantContracts = [
+        {
+          address: mockContractAddress,
+          amounts: [{ denom: "uxion", amount: "2000000" }],
+        },
+      ];
+
+      const result =
+        abstraxionAuth.compareGrantsToLegacyConfig(mockGrantsResponse);
+      expect(result).toBe(false);
+    });
+
+    it("should return true when legacy config stake changes from true to false", () => {
+      configureAbstraxionAuthInstance(abstraxionAuth);
+
+      abstraxionAuth.stake = false;
+
+      const result =
+        abstraxionAuth.compareGrantsToLegacyConfig(mockGrantsResponse);
+      expect(result).toBe(true);
+    });
+
+    it("should return false when the treasury instance has changed (grants returned from chain don't match)", async () => {
+      configureAbstraxionAuthInstance(abstraxionAuth);
+
+      abstraxionAuth.treasury =
+        "xion1qmxgcd4429ypkz7rejga2uya5fr88jlgkl03zvy2vg5e3y60hplq8yru7p"; // MsgSend w/ GenericAuth
+
+      cosmwasmClient.queryContractSmart
+        .mockResolvedValueOnce(["/cosmos.bank.v1beta1.MsgSend"])
+        .mockResolvedValueOnce({
+          authorization: {
+            type_url: "/cosmos.authz.v1beta1.GenericAuthorization",
+            value: { msg: "/cosmos.bank.v1beta1.MsgSend" }, // ChwvY29zbW9zLmJhbmsudjFiZXRhMS5Nc2dTZW5k
+          },
+        });
+
+      // Incorrect treasury config
+      jest
+        .spyOn(abstraxionAuth, "decodeAuthorization")
+        .mockImplementation((typeUrl: string, value: string) => {
+          if (typeUrl === "/cosmos.authz.v1beta1.GenericAuthorization") {
+            return { msg: "someIncorrectMsg" };
+          }
+          if (typeUrl === "/cosmos.bank.v1beta1.SendAuthorization") {
+            return {
+              spendLimit: "1000uxion",
+              allowList: ["address1"],
+            };
+          }
+          if (typeUrl === "/cosmos.staking.v1beta1.StakeAuthorization") {
+            return {
+              authorizationType: "1",
+              maxTokens: "5000uxion",
+              allowList: ["validator1"],
+              denyList: [],
+            };
+          }
+          return null;
+        });
+
+      const result =
+        await abstraxionAuth.compareGrantsToTreasury(mockGrantsResponse);
+      expect(result).toBe(false);
+    });
+
+    it("should return true when the treasury instance hasn't changed (grants returned from chain match)", async () => {
+      configureAbstraxionAuthInstance(abstraxionAuth);
+
+      abstraxionAuth.treasury =
+        "xion1qmxgcd4429ypkz7rejga2uya5fr88jlgkl03zvy2vg5e3y60hplq8yru7p"; // MsgSend w/ GenericAuth
+
+      cosmwasmClient.queryContractSmart
+        .mockResolvedValueOnce(["/cosmos.bank.v1beta1.MsgSend"])
+        .mockResolvedValueOnce({
+          authorization: {
+            type_url: "/cosmos.authz.v1beta1.GenericAuthorization",
+            value: { msg: "/cosmos.bank.v1beta1.MsgSend" }, // ChwvY29zbW9zLmJhbmsudjFiZXRhMS5Nc2dTZW5k
+          },
+        });
+
+      jest
+        .spyOn(abstraxionAuth, "decodeAuthorization")
+        .mockImplementation((typeUrl: string, value: string) => {
+          if (typeUrl === "/cosmos.authz.v1beta1.GenericAuthorization") {
+            return { msg: "/cosmwasm.wasm.v1.MsgSend" };
+          }
+          {
+            /* ... */
+          }
+          return null;
+        });
+
+      const result = await abstraxionAuth.compareGrantsToTreasury(
+        mockGrantsResponseForTreasury,
+      );
+      expect(result).toBe(true);
     });
   });
 });
